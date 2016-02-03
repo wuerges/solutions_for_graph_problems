75fd4fd14a6a07a290241bad00dc5366ecea4494
Emilio Wuerges <wuerges@gmail.com>
2016-02-03 17:56:12 -0200

    ok

--- app/Prob1119.hs
+++ app/Prob1119.hs
@@ -19,65 +19,65 @@ import qualified Data.Set as S
 type V = (Int, Int)
 type E = (V, V)
 
-data G = G { neighM :: M.Map V [(V, Double)], diag :: S.Set V, n_dim :: Int, m_dim :: Int }
+data G = G { removed :: S.Set V, diag :: S.Set V, n_dim :: Int, m_dim :: Int }
     deriving Show
 
 validV :: Int -> Int -> V -> Bool
 validV n m (a, b) = a >= 0 && b >= 0 && a <=n && b <= m
 
 sides :: V -> [V]
-sides v = [first (+1) v, first (1-) v, second (+1) v, second (1-) v]
-
-mkEdges :: [[Int]] -> Int -> Int -> [(E, Double)]
-mkEdges ds n m = des ++ ses
-    where des = zip [((x, y), (x-1, y-1)) | [x, y] <- ds] (repeat (sqrt 2))
-          es v = zip (repeat v) (filter (validV n m) (sides v))
-          vs = [(x, y) | x <- [0..n], y <- [0..m]]
-          ses = zip  (concatMap es vs) (repeat 1)
+--sides v = [first (+1) v, first (1-) v, second (+1) v, second (1-) v]
+sides v = [first (+1) v, second (+1) v]
 
 mkGraph :: [[Int]] -> Int -> Int -> G
-mkGraph ds n m = G { neighM = M.fromListWith (++) esN, diag = diags, n_dim = n, m_dim = m }
+mkGraph ds n m = G { removed = S.empty, diag = diags, n_dim = n, m_dim = m }
     where diags = S.fromList [(x, y) | [x, y] <- ds]
-          es1 = mkEdges ds n m
-          es2 = map (first swap) es1
-          es  = S.toList . S.fromList $ (es1 ++ es2)
-          esN = map (\((v1, v2), w) -> (v1, [(v2, w)])) es
 
 readInt :: String -> Int
 readInt = read
 
-decomp :: V -> G -> Maybe ([(V, Double)], G)
-decomp v g = do
-    ns <- M.lookup v (neighM g)
-    let g' = g { neighM = M.delete v (neighM g) }
-    return (ns, g')
+decomp :: V -> G -> [(V, Double)]
+decomp v g = ns
+       where ns = d2 ++ [(vi, 1) | vi <- sides v, validV (n_dim g) (m_dim g) vi]
+             d2 = if S.member (ru v) (diag g) then [(ru v, sqrt 2)] else []
+             ru (a, b) = (a+1, b+1)
 
 
-search :: G -> Q.Seq V -> Q.Seq (E, Double)
-search g q | Q.null q = Q.empty
-           | otherwise = case decomp v g of
-    Nothing -> search g vs
-    Just (ns, g') -> es >< search g' (vs >< qs)
-        where qs = Q.fromList $ map fst ds
-              ds = sortOn snd ns
-              es = Q.fromList $ [((a, b), w) | (a, (b, w)) <- zip (repeat v) ds]
-  where (v :< vs)  = Q.viewl q
+search :: G -> S.Set V -> Q.Seq V -> Q.Seq (E, Double)
+search g vis q | Q.null q       = Q.empty
+               | S.member v vis = search g vis vs
+               | otherwise =  es >< search g (S.insert v vis) (vs >< qs)
+                    where ns = decomp v g
+                          qs = Q.fromList $ map fst ds
+                          ds = sortOn snd ns
+                          es = Q.fromList $ [((a, b), w) | (a, (b, w)) <- zip (repeat v) ds]
+                          (v :< vs)  = Q.viewl q
 
 type Dst =  M.Map V (Double, V)
 
 calcSP :: Q.Seq (E, Double) -> Dst -> Dst
+calcSP q dst = foldl (flip calcSP1) dst q
+{-
 calcSP q rem | Q.null q  = rem
-             | otherwise = case M.lookup a rem of
-      Nothing     -> error "Could not find path for a"
-      Just (w, t) -> case M.lookup b rem of
-            Nothing -> calcSP es $ M.insert b (ew + w, a) rem
-            Just (ow, op) -> if ow > ew + w
-                                then calcSP es $ M.insert b (ew + w, a) rem
-                                else calcSP es $ rem
-  where (((a, b), ew) :< es) = Q.viewl q
+             | otherwise = calcSP es $ M.alter alterF b rem
+                      where (w, t) = rem ! a
+                            (((a, b), ew) :< es) = Q.viewl q
+                            alterF v = case v of
+                                       Nothing         -> Just (ew + w, a)
+                                       Just o@(ow, op) -> Just $ if ow > ew + w
+                                                           then (ew + w, a)
+                                                           else o
+                                                           -}
+
+calcSP1 :: (E, Double) -> Dst -> Dst
+calcSP1 ((a, b), ew) rem = M.alter alterF b rem
+    where (w, t) = rem ! a
+          alterF Nothing                         = Just (ew + w, a)
+          alterF (Just o@(ow, op)) | ow > ew + w = Just (ew + w, a)
+                                   | otherwise   = Just o
 
 shortestPaths :: G -> V -> Dst
-shortestPaths g v = calcSP (search g $ Q.singleton v) (M.singleton v (0.0, v))
+shortestPaths g v = calcSP (search g S.empty $ Q.singleton v) (M.singleton v (0.0, v))
 
 sortOn :: Ord b => (a -> b) -> [a] -> [a]
 sortOn = L.sortBy . comparing
